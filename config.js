a551768e2a5e78cd25172d1b36f5f2f5dea3513f
JrDev06 <jrbusaco72@gmail.com>
2024-06-10 14:46:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   host: 'RiseSMPMC.aternos.me',
   version: false, // Replace with a specific version, e.g., '1.16.5'
-  username: 'RiseSMPUptimeBOT',
+  username: 'RiseSMPUptimeBOT1',
   port: 46779,
   plugins: [AutoAuth],
-  AutoAuth: 'bot112022'
+  AutoAuth: 'bot06'
 };
