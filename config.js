61426fae55cadb0121301349f688759fe434331d
JrDev06 <jrbusaco72@gmail.com>
2024-06-10 14:42:39 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'RiseSMPMC.aternos.me',
+  version: false, // Replace with a specific version, e.g., '1.16.5'
+  username: 'RiseSMPUptimeBOT',
+  port: 46779,
+  plugins: [AutoAuth],
+  AutoAuth: 'bot112022'
+};
